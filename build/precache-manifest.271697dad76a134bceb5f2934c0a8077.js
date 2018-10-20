6a5516cd10a839a3e075a721ac8d5a1afb448db4
Abioye Bankole <bioyexx@yahoo.com>
2018-10-20 23:42:04 +0100

    built for production

---
+++ build/precache-manifest.271697dad76a134bceb5f2934c0a8077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0dbcab96bffcdc69baca",
+    "url": "/static/js/main.0dbcab96.chunk.js"
+  },
+  {
+    "revision": "b8c3d58f11471220fff9",
+    "url": "/static/js/1.b8c3d58f.chunk.js"
+  },
+  {
+    "revision": "0dbcab96bffcdc69baca",
+    "url": "/static/css/main.0019c322.chunk.css"
+  },
+  {
+    "revision": "b8c3d58f11471220fff9",
+    "url": "/static/css/1.7f99ead9.chunk.css"
+  },
+  {
+    "revision": "adf45908276eebc84ec7287957deed93",
+    "url": "/index.html"
+  }
+];
