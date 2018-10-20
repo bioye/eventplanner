662a0619a06f0c1b407d0535f309224d375d4d13
Abioye Bankole <bioyexx@yahoo.com>
2018-10-20 23:09:03 +0100

    updated mlab uri again.  hopefully..

--- server/database/DB.js
+++ server/database/DB.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb://heroku_5f17nx02:fF*12345@ds237563.mlab.com:37563/heroku_5f17nx02'
+    DB: 'mongodb://bioye:fF*12345@ds237563.mlab.com:37563/heroku_5f17nx02'
 }
